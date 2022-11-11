15fd42fdc9ad8f202b7a072b499b6cc50a901ab4
Moacir Junior <skytec.dev@gmail.com>
2022-11-11 20:36:23 -0300

    initial commit - jpa extended

---
+++ src/main/java/com/github/moacirlsj/citiesapi/repository/StateRepository.java
@@ -0,0 +1,8 @@
+package com.github.moacirlsj.citiesapi.repository;
+
+import com.github.moacirlsj.citiesapi.entity.State;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StateRepository extends JpaRepository<State, Long> {
+
+}
