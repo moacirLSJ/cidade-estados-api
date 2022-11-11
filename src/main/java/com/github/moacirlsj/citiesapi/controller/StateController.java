9a0408c52ce4fa8114ab63bb641acff9d7a6a0ef
Moacir Junior <skytec.dev@gmail.com>
2022-11-11 20:33:53 -0300

    initial commit with restcontroller annotation

---
+++ src/main/java/com/github/moacirlsj/citiesapi/controller/StateController.java
@@ -0,0 +1,10 @@
+package com.github.moacirlsj.citiesapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/state")
+public class StateController {
+
+}
