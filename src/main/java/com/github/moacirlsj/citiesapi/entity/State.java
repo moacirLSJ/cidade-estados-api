5880df82b5d8d7d244b1c7c44b59ec7df75f75b0
Moacir Junior <skytec.dev@gmail.com>
2022-11-11 20:31:10 -0300

    type def dependency

--- src/main/java/com/github/moacirlsj/citiesapi/entity/State.java
+++ src/main/java/com/github/moacirlsj/citiesapi/entity/State.java
@@ -1,14 +1,32 @@
 package com.github.moacirlsj.citiesapi.entity;
 
+import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
+
 import javax.persistence.*;
+import java.util.List;
 
 @Entity(name = "State")
 @Table(name = "estado")
+@TypeDefs({
+        @TypeDef(name = "jsonb", typeClass = JsonBinaryType.class)
+})
 public class State {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
+
+    private String nome;
+    private String uf;
+    private Integer ibge;
+    private Integer pais;
+    @Type(type = "jsonb")
+    @Basic(fetch = FetchType.LAZY)
+    @Column(columnDefinition = "jsonb")
+    private List<Integer> ddd;
 
     public Long getId() {
         return id;
