a43e1a84d7e2f907f80398374cfa58eb43f9fdca
Moacir Junior <skytec.dev@gmail.com>
2022-11-11 20:42:12 -0300

    get all state done

--- src/main/java/com/github/moacirlsj/citiesapi/controller/StateController.java
+++ src/main/java/com/github/moacirlsj/citiesapi/controller/StateController.java
@@ -1,10 +1,24 @@
 package com.github.moacirlsj.citiesapi.controller;
 
+import com.github.moacirlsj.citiesapi.entity.State;
+import com.github.moacirlsj.citiesapi.repository.StateRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/state")
 public class StateController {
+
+    private StateRepository stateRepository;
+
+
+    @GetMapping
+    public Page<State> getAllState(final Pageable page) {
+        return stateRepository.findAll(page);
+    }
+
 
 }
