d9d3a2513b33027b4b581312a4073b37bd4f879d
Moacir Junior <skytec.dev@gmail.com>
2022-11-11 21:15:39 -0300

    get by id with ResponseEntity

--- src/main/java/com/github/moacirlsj/citiesapi/controller/StateController.java
+++ src/main/java/com/github/moacirlsj/citiesapi/controller/StateController.java
@@ -4,11 +4,14 @@ import com.github.moacirlsj.citiesapi.entity.State;
 import com.github.moacirlsj.citiesapi.repository.StateRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/v1/state")
@@ -24,6 +27,16 @@ public class StateController {
     @GetMapping
     public Page<State> getAllState(final Pageable page) {
         return stateRepository.findAll(page);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getStateById(@PathVariable Long id) {
+        Optional<State> optionalState = stateRepository.findById(id);
+        if (optionalState.isPresent()) {
+            return ResponseEntity.ok().body(optionalState.get());
+        }else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 
